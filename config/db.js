9be1814cea9f2b211279c9f469ccc5b8c9a5bba9
Shashikant Rupin <shashikantrupin123@gmail.com>
2024-02-07 22:35:23 +0530

    error-resolved

--- config/db.js
+++ config/db.js
@@ -2,6 +2,9 @@ const mongoose=require('mongoose');
 require("dotenv").config();
 //connection
 const MONGO_URL = process.env.MONGO_URL;
-const connection = mongoose.connect(`${MONGO_URL}`);
+const connection = mongoose.connect(`${MONGO_URL}`, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-module.exports={connection};
+module.exports = { connection };
